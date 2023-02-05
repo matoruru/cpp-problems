a8c8d5289ff4d4cc3fe009e9ac592840345ccb71
matoruru <matoruru@shgt.jp>
2023-02-05 23:17:57 +0900

    Added my solutions

--- AtCoder_Problems/easy/024/main.cpp
+++ AtCoder_Problems/easy/024/main.cpp
@@ -1,11 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define rep(i, c) for(int i = 0; i < (int)c; i++)
 
 int main() {
-  int A, B;
-  cin >> A >> B;
+  int A, B, M; cin >> A >> B >> M;
+  vector<int> a(A), b(B);
+  rep(i, A) cin >> a.at(i);
+  rep(i, B) cin >> b.at(i);
 
-  //cout << /*answer*/ << endl;
+
+  int aMin = *min_element(a.begin(), a.end());
+  int bMin = *min_element(b.begin(), b.end());
+
+  int ans = aMin + bMin;
+  for (int i = 0; i < M; i++) {
+    int x, y, c; cin >> x >> y >> c;
+    int withCoupon = a.at(x - 1) + b.at(y - 1) - c;
+    ans = min(ans, withCoupon);
+  }
+
+  cout << ans << endl;
 
   return 0;
 }
--- AtCoder_Problems/easy/025/main.cpp
+++ AtCoder_Problems/easy/025/main.cpp
@@ -1,11 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define rep(i, c) for(int i = 0; i < (int)c; i++)
+
+int getDictionaryOrder(vector<int> sorted, vector<int> &v, int N) {
+  int count = 0;
+  do {
+    int isSame = true;
+    count++;
+    for (int i = 0; i < N; i++) {
+      if (sorted.at(i) != v.at(i)) {
+        isSame = false;
+        break;
+      }
+    }
+    if (isSame) return count;
+  } while (next_permutation(sorted.begin(), sorted.end()));
+}
 
 int main() {
-  int A, B;
-  cin >> A >> B;
+  int N; cin >> N;
+  vector<int> P(N), Q(N), sorted(N);
+  rep(i, N) cin >> P.at(i);
+  rep(i, N) cin >> Q.at(i);
+  rep(i, N) sorted.at(i) = i + 1;
 
-  //cout << /*answer*/ << endl;
+  cout << abs(getDictionaryOrder(sorted, P, N) - getDictionaryOrder(sorted, Q, N)) << endl;
 
   return 0;
 }
