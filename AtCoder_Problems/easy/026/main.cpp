a2892d9d4f1dcb1bae64e107ef64f300001291d5
matoruru <matoruru@shgt.jp>
2023-02-06 21:23:36 +0900

    Added my solutions

--- AtCoder_Problems/easy/026/main.cpp
+++ AtCoder_Problems/easy/026/main.cpp
@@ -1,11 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-  int A, B;
-  cin >> A >> B;
+using ll = long long;
 
-  //cout << /*answer*/ << endl;
+int main() {
+  ll H; cin >> H;
+
+  ll count = 1, ans = 0;
+  while (H > 0) {
+    ans += count;
+    H /= 2;
+    count = count * 2;
+  }
+
+  cout << ans << endl;
 
   return 0;
 }
