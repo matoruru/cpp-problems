fe34bb6ffb120930f0d1001a91469c9625812629
matoruru <matoruru@shgt.jp>
2023-02-09 23:29:40 +0900

    Added my solutions

--- AtCoder_Problems/easy/038/main.cpp
+++ AtCoder_Problems/easy/038/main.cpp
@@ -1,11 +1,43 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-  int A, B;
-  cin >> A >> B;
+int calcLastOrderIdx(vector<int> order) {
+  int min = 10;
+  int lastOrderIdx = 0;
+  for (int i = 0; i < 5; i++) {
+    int mod10 = order.at(i) % 10;
+    if (mod10 > 0 && min > mod10) {
+      min = mod10;
+      lastOrderIdx = i;
+    }
+  }
+  return lastOrderIdx;
+}
 
-  //cout << /*answer*/ << endl;
+int nextOrderTime(int t) {
+  while(t % 10 != 0) t++;
+  return t;
+}
+
+void swap(int &a, int &b) {
+  int tmp = a;
+  a = b;
+  b = tmp;
+}
+
+int main() {
+  vector<int> order(5);
+  for (int i = 0; i < 5; i++) {
+    cin >> order.at(i);
+  }
+  swap(order.at(calcLastOrderIdx(order)), order.at(4));
+
+  int time = 0;
+  for (int i = 0; i < 4; i++) {
+    time += nextOrderTime(order.at(i));
+  }
+
+  cout << time + order.at(4) << endl;
 
   return 0;
 }
