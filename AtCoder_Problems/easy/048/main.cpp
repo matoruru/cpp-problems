b23819c6b12e11cac0f52e6568cbc85bdb37e654
matoruru <matoruru@shgt.jp>
2023-02-13 22:28:39 +0900

    Added my solutions

--- AtCoder_Problems/easy/048/main.cpp
+++ AtCoder_Problems/easy/048/main.cpp
@@ -1,11 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-  int A, B;
-  cin >> A >> B;
+using ll = long long;
 
-  //cout << /*answer*/ << endl;
+int main() {
+  int N;
+  cin >> N;
+  vector<ll> H(N);
+  for (int i = 0; i < N; i++)
+    cin >> H.at(i);
+
+  int countMax = 0, count;
+  for (int i = 0; i < N; i++) {
+    count = 0;
+    for (int j = i + 1; j < N; j++) {
+      if (H.at(j - 1) < H.at(j))
+        break;
+      count++;
+    }
+    countMax = max(countMax, count);
+    i += count;
+  }
+
+  cout << countMax << endl;
 
   return 0;
 }
