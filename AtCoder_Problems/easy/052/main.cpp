693b0002c50ecb6fb0006295caaab78e212cb6bf
matoruru <matoruru@shgt.jp>
2023-02-14 23:52:10 +0900

    Added my solutions

--- AtCoder_Problems/easy/052/main.cpp
+++ AtCoder_Problems/easy/052/main.cpp
@@ -2,10 +2,20 @@
 using namespace std;
 
 int main() {
-  int A, B;
-  cin >> A >> B;
+  int N;
+  cin >> N;
+  vector<int> B(N + 1);
+  B.at(0) = B.at(N) = INT_MAX;
+  for (int i = 1; i < N; i++) {
+    cin >> B.at(i);
+  }
 
-  //cout << /*answer*/ << endl;
+  int total = 0;
+  for (int i = 0; i < N; i++) {
+    total += min(B.at(i), B.at(i + 1));
+  }
+
+  cout << total << endl;
 
   return 0;
 }
--- AtCoder_Problems/easy/053/main.cpp
+++ AtCoder_Problems/easy/053/main.cpp
@@ -2,10 +2,19 @@
 using namespace std;
 
 int main() {
-  int A, B;
-  cin >> A >> B;
+  int N, M;
+  cin >> N >> M;
+  vector<int> roads(N + 1, 0);
 
-  //cout << /*answer*/ << endl;
+  for (int i = 0; i < M; i++) {
+    int ai, bi;
+    cin >> ai >> bi;
+    roads.at(ai)++;
+    roads.at(bi)++;
+  }
+
+  for (int i = 1; i < N + 1; i++)
+    cout << roads.at(i) << endl;
 
   return 0;
 }
