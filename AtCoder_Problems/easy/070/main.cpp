43973693ac3dd6eb7c8741e7143039a28bd86b47
matoruru <matoruru@shgt.jp>
2023-02-17 16:15:45 +0000

    Added my solutions

--- AtCoder_Problems/easy/070/main.cpp
+++ AtCoder_Problems/easy/070/main.cpp
@@ -1,11 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-  int A, B;
-  cin >> A >> B;
+using ll = long long;
 
-  //cout << /*answer*/ << endl;
+int main() {
+  ll X;
+  cin >> X;
+
+  for (int i = 1; X >= i * 100; i++) {
+    if (i * 100 <= X && X <= i * 100 + i * 5) {
+      cout << 1 << endl;
+      return 0;
+    }
+  }
+
+  cout << 0 << endl;
 
   return 0;
 }
