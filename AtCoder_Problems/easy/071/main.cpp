6a8f3e5ff0111e72ed53e46cc81f7f06c3ec8550
matoruru <matoruru@shgt.jp>
2023-02-18 12:30:29 +0000

    Added my solutions

--- AtCoder_Problems/easy/071/main.cpp
+++ AtCoder_Problems/easy/071/main.cpp
@@ -2,5 +2,16 @@
 using namespace std;
 
 int main() {
+  int N, L;
+  cin >> N >> L;
+  vector<string> S(N);
+  for (int i = 0; i < N; i++) {
+    cin >> S.at(i);
+  }
+  sort(S.begin(), S.end());
+  for (auto Si : S) {
+    cout << Si;
+  }
+  cout << endl;
   return 0;
 }
--- AtCoder_Problems/easy/072/main.cpp
+++ AtCoder_Problems/easy/072/main.cpp
@@ -2,5 +2,19 @@
 using namespace std;
 
 int main() {
+  int X;
+  cin >> X;
+
+  int max_ = 1;
+  for (int i = 2; i < X; i++) {
+    int acc = i * i;
+    while (acc <= X) {
+      max_ = max(max_, acc);
+      acc *= i;
+    }
+  }
+
+  cout << max_ << endl;
+
   return 0;
 }
