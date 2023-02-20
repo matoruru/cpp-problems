0c26b7468d04ab250bd07057cf69d38acb24eb12
matoruru <matoruru@shgt.jp>
2023-02-20 12:36:32 +0000

    Added my solutions

--- AtCoder_Problems/easy/074/main.cpp
+++ AtCoder_Problems/easy/074/main.cpp
@@ -1,6 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+using ll = long long;
+
 int main() {
+  ll A, B, C, X, Y;
+  cin >> A >> B >> C >> X >> Y;
+
+  ll min_ = INT_MAX;
+  for (int i = 0; i < 300000; i++) {
+    ll C_cost = C * 2 * i;
+    ll A_cost = A * max(0LL, (X - i));
+    ll B_cost = B * max(0LL, (Y - i));
+    min_ = min(min_, C_cost + A_cost + B_cost);
+    if (X - i == 0 && Y - i == 0)
+      break;
+  }
+
+  cout << min_ << endl;
+
   return 0;
 }
--- AtCoder_Problems/easy/075/main.cpp
+++ AtCoder_Problems/easy/075/main.cpp
@@ -1,6 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+using ll = long long;
+
+template<class T> inline bool chmin(T &a, T b) {
+  if (a > b) { a = b; return true; }
+  return false;
+}
+
 int main() {
+  int N, K;
+  cin >> N >> K;
+  vector<ll> h(N);
+  for (int i = 0; i < N; i++)
+    cin >> h.at(i);
+  sort(h.begin(), h.end());
+  ll min = INT_MAX;
+  for (int i = 0; i <= N - K; i++) {
+    int L = i, R = K + i - 1;
+    chmin(min, h.at(R) - h.at(L));
+  }
+
+  cout << min << endl;
+
   return 0;
 }
