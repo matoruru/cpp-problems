b3d11c010e35298bc2a95542df3ce8ce2afd143f
matoruru <matoruru@shgt.jp>
2023-02-21 13:37:30 +0000

    Added my solutions

--- AtCoder_Problems/easy/078/main.cpp
+++ AtCoder_Problems/easy/078/main.cpp
@@ -1,6 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+using ll = long long;
+
 int main() {
+  ll N;
+  cin >> N;
+  vector<ll> H(N);
+  for (int i = 0; i < N; i++) {
+    cin >> H.at(i);
+  }
+  for (int i = N - 1; i > 0; i--) {
+    auto check = [&]() { return H.at(i) < H.at(i - 1); };
+    if (check()) {
+      H.at(i - 1)--;
+      if (check()) {
+        cout << "No" << endl;
+        return 0;
+      }
+    }
+  }
+  cout << "Yes" << endl;
   return 0;
 }
--- AtCoder_Problems/easy/079/main.cpp
+++ AtCoder_Problems/easy/079/main.cpp
@@ -1,6 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+using ll = long long;
+
 int main() {
+  ll N, A, B;
+  cin >> N >> A >> B;
+
+  ll diff = B - A;
+  if (diff % 2 == 0) {
+    cout << diff / 2 << endl;
+  } else {
+    cout << min(A - 1,  N - B) + 1 + ((B - A - 1) / 2) << endl;
+  }
+  
   return 0;
 }
