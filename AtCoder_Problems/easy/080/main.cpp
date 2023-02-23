7ee7708ff9fcd350dccc24f27a9cf430d56b2c1f
matoruru <matoruru@shgt.jp>
2023-02-23 13:27:12 +0000

    Added my solutions

--- AtCoder_Problems/easy/080/main.cpp
+++ AtCoder_Problems/easy/080/main.cpp
@@ -1,6 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+using ll = long long;
+
 int main() {
+  ll N;
+  cin >> N;
+  ll min = LONG_LONG_MAX;
+  for (int i = 1; i <= 1000000; i++) {
+    if (N % i == 0) {
+      ll j = N / i;
+      min = std::min(min, i + j - 2);
+    }
+  }
+
+  cout << min << endl;
+
   return 0;
 }
