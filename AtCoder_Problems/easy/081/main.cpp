4e4e241d224e7f34a8c367a703124801c7a54ed4
matoruru <matoruru@shgt.jp>
2023-02-25 09:35:05 +0000

    Added my solutions

--- AtCoder_Problems/easy/081/main.cpp
+++ AtCoder_Problems/easy/081/main.cpp
@@ -1,6 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+using ll = long long;
+
+int getHowMany2(int a) {
+  int cnt = 0;
+  while(a % 2 == 0) {
+    a /= 2;
+    cnt++;
+  }
+  return cnt;
+}
+
 int main() {
+  int N;
+  cin >> N;
+  vector<int> a(N);
+  for (int i = 0; i < N; i++) {
+    cin >> a.at(i);
+  }
+
+  int count2 = 0;
+  for (int i = 0; i < N; i++) {
+    count2 += getHowMany2(a.at(i));
+  }
+
+  cout << count2 << endl;
+
   return 0;
 }
