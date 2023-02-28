b7ec8e08884a00fc80bd575fd02401aa597f1368
matoruru <matoruru@shgt.jp>
2023-02-28 15:52:43 +0000

    Added my solutions

--- AtCoder_Problems/easy/088/main.cpp
+++ AtCoder_Problems/easy/088/main.cpp
@@ -2,5 +2,34 @@
 using namespace std;
 
 int main() {
+  int N;
+  map<string, int> m;
+  cin >> N;
+  for (int i = 0; i < N; i++) {
+    string s;
+    cin >> s;
+    if (!m.count(s)) {
+      m.insert(pair(s, 0));
+    }
+    m.at(s)++;
+  }
+  int M;
+  cin >> M;
+  for (int i = 0; i < M; i++) {
+    string t;
+    cin >> t;
+    if (!m.count(t)) {
+      m.insert(pair(t, 0));
+    }
+    m.at(t)--;
+  }
+
+  int max = 0;
+  for (auto ma : m) {
+    max = std::max(max, ma.second);
+  }
+
+  cout << max << endl;
+
   return 0;
 }
--- AtCoder_Problems/easy/089/main.cpp
+++ AtCoder_Problems/easy/089/main.cpp
@@ -1,6 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+using ll = long long;
+
 int main() {
+  ll X, Y;
+  cin >> X >> Y;
+  int count = 1;
+  while (X * 2 <= Y) {
+    X *= 2;
+    count++;
+  }
+  cout << count << endl;
   return 0;
 }
