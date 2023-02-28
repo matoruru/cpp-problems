71746427c7b710430ce518caeff1800420984c47
matoruru <matoruru@shgt.jp>
2023-02-28 12:37:46 +0000

    Added my solutions

--- AtCoder_Problems/easy/083/main.cpp
+++ AtCoder_Problems/easy/083/main.cpp
@@ -2,5 +2,26 @@
 using namespace std;
 
 int main() {
+  int N;
+  cin >> N;
+  vector<vector<int>> A(2, vector<int>(N));
+  for (int i = 0; i < 2; i++) {
+    for (int j = 0; j < N; j++) {
+      cin >> A.at(i).at(j);
+    }
+  }
+  int max = INT_MIN;
+  for (int i = 0; i < N; i++) {
+    int total = 0, lr = 0;
+    for (int j = 0; j < N; j++) {
+      if (i == j) {
+        total += A.at(lr).at(j);
+        lr = 1;
+      }
+      total += A.at(lr).at(j);
+    }
+    max = std::max(max, total);
+  }
+  cout << max << endl;
   return 0;
 }
--- AtCoder_Problems/easy/084/main.cpp
+++ AtCoder_Problems/easy/084/main.cpp
@@ -1,6 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+using ll = long long;
+
 int main() {
+  ll N;
+  cin >> N;
+  vector<ll> A(N + 1), B(N);
+  for (ll i = 0; i < N + 1; i++)
+    cin >> A.at(i);
+  for (ll i = 0; i < N; i++)
+    cin >> B.at(i);
+
+  ll total = 0;
+  for (ll i = 0; i < N; i++) {
+    ll l = min(A.at(i), B.at(i));
+    ll r = min(A.at(i + 1), B.at(i) - l);
+    A.at(i + 1) -= r;
+    total += l + r;
+  }
+
+  cout << total << endl;
+
   return 0;
 }
