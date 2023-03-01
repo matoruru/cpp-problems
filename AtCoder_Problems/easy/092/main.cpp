57c78cf9e4d536569fa47759458d9c9f2b2119e8
matoruru <matoruru@shgt.jp>
2023-03-01 14:22:07 +0000

    Added my solutions

--- AtCoder_Problems/easy/092/main.cpp
+++ AtCoder_Problems/easy/092/main.cpp
@@ -2,5 +2,16 @@
 using namespace std;
 
 int main() {
+  string S;
+  cin >> S;
+  int count0 = 0, count1 = 0;
+  for (int i = 0; i < S.size(); i++) {
+    char c = S.at(i);
+    if (c == '0')
+      count0++;
+    else
+      count1++;
+  }
+  cout << min(count0, count1) * 2 << endl;
   return 0;
 }
--- AtCoder_Problems/easy/093/main.cpp
+++ AtCoder_Problems/easy/093/main.cpp
@@ -1,6 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+using ll = long long;
+
 int main() {
+  int N;
+  cin >> N;
+  vector<ll> A(N), sumMemo(N);
+  for (int i = 0; i < N; i++) {
+    cin >> A.at(i);
+  }
+
+  sumMemo.at(0) = A.at(0);
+  for (int i = 1; i < N; i++) {
+    sumMemo.at(i) = sumMemo.at(i - 1) + A.at(i);
+  }
+
+  ll min = INT_MAX;
+  for (int i = 0; i < N; i++) {
+    min = std::min(min, abs(sumMemo.at(i) - (sumMemo.at(N - 1) - sumMemo.at(i))));
+  }
+
+  cout << min << endl;
+
   return 0;
 }
