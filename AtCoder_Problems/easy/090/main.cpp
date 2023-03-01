60c93275cbb6ba7b53e4f5c009d51428d675d4c3
matoruru <matoruru@shgt.jp>
2023-03-01 10:17:34 +0000

    Added my solutions

--- AtCoder_Problems/easy/090/main.cpp
+++ AtCoder_Problems/easy/090/main.cpp
@@ -1,6 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+using ll = long long;
+
 int main() {
+  ll N;
+  cin >> N;
+  ll ans = 1;
+  for (int i = 1; i <= N; i++) {
+    ans = i * ans % (1000000000 + 7);
+  }
+  cout << ans << endl;
   return 0;
 }
--- AtCoder_Problems/easy/091/main.cpp
+++ AtCoder_Problems/easy/091/main.cpp
@@ -2,5 +2,26 @@
 using namespace std;
 
 int main() {
+  int N;
+  cin >> N;
+  vector<int> x(N), y(N), ord(N);
+  for (int i = 0; i < N; i++) {
+    cin >> x.at(i) >> y.at(i);
+    ord.at(i) = i;
+  }
+
+  double total = 0.0;
+  int count = 0;
+  do {
+    for (int i = 0; i < N - 1; i++) {
+      int dx = x.at(ord.at(i)) - x.at(ord.at(i + 1));
+      int dy = y.at(ord.at(i)) - y.at(ord.at(i + 1));
+      total += sqrt(dx * dx + dy * dy);
+    }
+    count++;
+  } while (next_permutation(ord.begin(), ord.end()));
+
+  cout << setprecision(10) << total / count << endl;
+
   return 0;
 }
