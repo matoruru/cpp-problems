4ce130aeb55c25a4a1a3061a4983da6c392fbaea
matoruru <matoruru@shgt.jp>
2023-03-03 14:33:48 +0000

    Added my solutions

--- AtCoder_Problems/easy/096/main.cpp
+++ AtCoder_Problems/easy/096/main.cpp
@@ -1,6 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int pow(int a, int b) {
+  int x = 1;
+  while (b--) x *= a;
+  return x;
+}
+
 int main() {
+  int N;
+  cin >> N;
+  int odd = 0;
+  for (int i = 0; i < N; i++) {
+    int a;
+    cin >> a;
+    if (a % 2 == 0) odd++;
+  }
+  cout << pow(3, N) - pow(2, odd) << endl;
   return 0;
 }
--- AtCoder_Problems/easy/097/main.cpp
+++ AtCoder_Problems/easy/097/main.cpp
@@ -1,6 +1,34 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+using ll = long long;
+
 int main() {
+  int N, M;
+  cin >> N >> M;
+  vector<int> A(N), B(N);
+  vector<int> cheapOrd(N);
+  for (int i = 0; i < N; i++) {
+    cheapOrd.at(i) = i;
+    cin >> A.at(i) >> B.at(i);
+  }
+
+  sort(cheapOrd.begin(), cheapOrd.end(), [&](const int &a, const int &b) {
+    return A.at(a) < A.at(b);
+  });
+
+  ll cost = 0;
+  for (int i = 0; i < N; i++) {
+    int idx = cheapOrd.at(i);
+    ll num = min(B.at(idx), M);
+    cost += A.at(idx) * num;
+    M -= num;
+    if (M == 0) {
+      break;
+    }
+  }
+
+  cout << cost << endl;
+
   return 0;
 }
