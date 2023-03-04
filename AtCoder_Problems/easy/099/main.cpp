04aa2b4cbf1a9acee722d4d57e47319c4ed168ab
matoruru <matoruru@shgt.jp>
2023-03-04 15:35:19 +0000

    Added my solutions

--- AtCoder_Problems/easy/099/main.cpp
+++ AtCoder_Problems/easy/099/main.cpp
@@ -1,6 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+using ll = long long;
+
 int main() {
+  ll N, K;
+  cin >> N >> K;
+
+  ll ans = K;
+  for (int i = 1; i < N; i++) {
+    ans *= K - 1;
+  }
+
+  cout << ans << endl;
+
   return 0;
 }
--- AtCoder_Problems/easy/100/main.cpp
+++ AtCoder_Problems/easy/100/main.cpp
@@ -2,5 +2,21 @@
 using namespace std;
 
 int main() {
+  int N;
+  cin >> N;
+  vector<int> a(N + 1);
+  for (int i = 1; i <= N; i++) {
+    cin >> a.at(i);
+  }
+
+  int count = 0;
+  for (int i = 1; i <= N; i++) {
+    int ai = a.at(a.at(i));
+    if (ai == i)
+      count++;
+  }
+
+  cout << count / 2 << endl;
+
   return 0;
 }
