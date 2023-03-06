b5eb6985cbd1276c2f552d344e276d8ed713b92e
matoruru <matoruru@shgt.jp>
2023-03-06 14:46:32 +0000

    Added my solutions

--- AtCoder_Problems/medium/002/main.cpp
+++ AtCoder_Problems/medium/002/main.cpp
@@ -1,6 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+using ll = long long;
+
 int main() {
+  string S;
+  cin >> S;
+  ll len = S.size();
+  ll wDistance = 0;
+
+  ll ans = 0;
+  for (int i = 0; i < len; i++) {
+    if (S.at(i) == 'W')
+      ans += wDistance;
+    else
+      wDistance++;
+  }
+
+  cout << ans << endl;
+
   return 0;
 }
