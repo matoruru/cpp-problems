7efae353d3d7b1eec4ec9fe1a737b93eb5f4fc4e
matoruru <matoruru@shgt.jp>
2023-03-07 14:59:09 +0000

    Added my solutions

--- Tessoku-book/A02/main.cpp
+++ Tessoku-book/A02/main.cpp
@@ -2,4 +2,18 @@
 using namespace std;
 
 int main() {
+  int N, X;
+  cin >> N >> X;
+
+  for (int i = 0; i < N; i++) {
+    int a;
+    cin >> a;
+    if (a == X) {
+      cout << "Yes" << endl;
+      return 0;
+    }
+  }
+
+  cout << "No" << endl;
+  return 0;
 }
