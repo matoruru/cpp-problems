0856079daa9803b9b9f8bcbef36b4fed8a91ff55
matoruru <matoruru@shgt.jp>
2023-03-25 12:14:39 +0000

    Added my solutions

--- Tessoku-book/A04/main.cpp
+++ Tessoku-book/A04/main.cpp
@@ -1,6 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+string solve(int n) {
+  string s = "";
+  int k = 512;
+  while (k >= 1) {
+    if (n >= k) {
+      n -= k;
+      s += "1";
+    } else {
+      s += "0";
+    }
+    k /= 2;
+  }
+  return s;
+}
+
 int main() {
+  int N;
+  cin >> N;
+  cout << solve(N) << endl;
   return 0;
 }
--- Tessoku-book/A05/main.cpp
+++ Tessoku-book/A05/main.cpp
@@ -2,5 +2,17 @@
 using namespace std;
 
 int main() {
+  int N, K;
+  cin >> N >> K;
+  int count = 0;
+  for (int i = 1; i <= N; i++) {
+    for (int j = 1; j <= N; j++) {
+      int x = K - i - j;
+      if (x >= 1 && x <= N) {
+        count++;
+      }
+    }
+  }
+  cout << count << endl;
   return 0;
 }
